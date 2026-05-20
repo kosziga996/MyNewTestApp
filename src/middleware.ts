aadea92582ea87f817dd8ce9ab52c86ed3a7fb0f
Ziga Kos <ziga.kos@dasof.si>
2026-05-20 16:02:00 +0200

    unprotect pages (for now)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import { defineMiddleware } from "astro:middleware";
 import { auth } from "./lib/auth";
 
-const PUBLIC_PATHS = ["/signin", "/api/auth"];
-
 export const onRequest = defineMiddleware(async (context, next) => {
   const session = await auth.api.getSession({
     headers: context.request.headers,
@@ -10,13 +8,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
   context.locals.session = session?.session ?? null;
   context.locals.user = session?.user ?? null;
-
-  const { pathname } = context.url;
-  const isPublic = PUBLIC_PATHS.some((p) => pathname === p || pathname.startsWith(p + "/"));
-
-  if (!session?.user && !isPublic) {
-    return context.redirect("/signin");
-  }
 
   return next();
 });
